073ae6001a300b8d24b4762631ed4263665408bd
Alexander Polovinka <apolovinka@gmail.com>
2019-02-28 15:27:10 +0200

    - footer view, safeAreaInset updated;

--- AssetsPickerViewController/Classes/Photo/View/AssetsFooterView.swift
+++ AssetsPickerViewController/Classes/Photo/View/AssetsFooterView.swift
@@ -16,10 +16,12 @@ open class AssetsPhotoFooterView: UICollectionReusableView {
             if let contentInset = self.contentInset {
                 topInset = contentInset.bottom
             }
-            countLabel.topConstraint.updateOffset(topInset)
+            self.topConstraint?.constant = topInset
         }
     }
-    
+
+    private var topConstraint: NSLayoutConstraint?
+
     private let countLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -27,25 +29,29 @@ open class AssetsPhotoFooterView: UICollectionReusableView {
         label.textColor = .darkText
         return label
     }()
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
-    
+
     private func commonInit() {
         addSubview(countLabel)
-        countLabel.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
-        }
+
+        countLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.topConstraint = countLabel.topAnchor.constraint(equalTo: self.topAnchor)
+        self.topConstraint?.isActive = true
+        countLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        countLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        countLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
 
-    
+
     open func set(imageCount: Int, videoCount: Int) {
         var countText: String?
         if imageCount > 0 && videoCount > 0 {
